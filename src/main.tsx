48f8fc40519f7acbc2ffacabd178d37c9019c0a8
unknown <fikriamd12@gmail.com>
2024-10-27 16:57:28 -1200

    add layout for admin and added sidebar to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import LandingPage from "./pages/LandingPage.tsx";
 import "./styles/index.css";
 
 import Dashboard from "./pages/Profile/Seller/Dashboard.tsx";
