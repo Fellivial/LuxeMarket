8e8db418ce5d9c57fb51d8cd814904f463771beb
unknown <fikriamd12@gmail.com>
2024-11-14 14:45:17 -1200

    add cardstat added in dashboard

---
+++ src/component/Card/CardStat.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface CardStatProps {
+  id: number;
+  title: string;
+  value: string;
+}
+const CardStat = ({ id, title, value }: CardStatProps) => {
+  return (
+    <div key={id} className="bg-white border shadow rounded-lg py-2 px-3 my-4">
+      <div className="mx-auto max-w-3xl px-3">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-16">
+          <div className="mx-auto flex max-w-xs flex-col gap-y-1">
+            <p className="text-base/7 text-gray-600">{title}</p>
+            <p className="text-3xl font-semibold tracking-tight text-gray-900">
+              {value}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardStat;
--- src/pages/Profile/Seller/Dashboard.tsx
+++ src/pages/Profile/Seller/Dashboard.tsx
@@ -1,10 +1,43 @@
 import LayoutAdmin from "../../../component/Layout/LayoutAdmin";
 
+import CardStat from "../../../component/Card/CardStat";
+
+const stats = [
+  {
+    id: 1,
+    title: "Total User",
+    value: "40,768",
+  },
+  {
+    id: 2,
+    title: "Total Order",
+    value: "18796",
+  },
+  {
+    id: 3,
+    title: "Total Sales",
+    value: "$89,000",
+  },
+  {
+    id: 4,
+    title: "Total Profit",
+    value: "$10,890",
+  },
+  {
+    id: 5,
+    title: "Today",
+    value: "$4,000",
+  },
+];
 const Dashboard = () => {
   return (
     <LayoutAdmin>
       <div className="flex flex-row justify-center items-center">
-        <div>hallo</div>
+        <div className="flex flex-row justify-around space-x-3 py-4">
+          {stats.map((stat) => (
+            <CardStat id={stat.id} title={stat.title} value={stat.value} />
+          ))}
+        </div>
       </div>
     </LayoutAdmin>
   );
